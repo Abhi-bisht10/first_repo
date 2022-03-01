ca7cb21ab532e7cbbbe01abb580ec79b83eb0680
Aayush Singh <ayush.singh94115@gmail.com>
2022-03-01 16:37:32 +0530

    ncr program

---
+++ ncr.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int factorial(int a)
+{
+    int fact=1;
+    while(a>0)
+    {
+        fact=fact*a;
+        a--;
+    }
+
+    return fact;
+}
+void main()
+{
+    int n,r;
+    int o;
+    scanf("%d%d",&n,&r);
+    o=factorial(n)/(factorial(r)*factorial(n-r));
+    printf("%d",o);
+
+}
